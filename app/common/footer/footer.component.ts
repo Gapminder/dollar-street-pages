2cdd4fa5b6d1ed4b61bdc90b6ce7ff7321e1e5c5
nutman <juncker88@mail.ru>
2016-02-15 14:45:28 +0200

    Fix: change name space for footer

--- app/common/footer/footer.component.ts
+++ app/common/footer/footer.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from 'angular2/core';
 import {SocialShareButtons} from '../social_share_buttons/social_share_buttons.component';
 
-let tpl = require('./footer.main.component.html');
-let style = require('./footer.main.component.css');
+let tpl = require('./footer.component.html');
+let style = require('./footer.component.css');
 
 @Component({
-  selector: 'footer-main',
+  selector: 'footer',
   template: tpl,
   styles: [style],
   directives: [SocialShareButtons]
 })
 
-export class FooterMainComponent {
+export class FooterComponent {
 
 }
