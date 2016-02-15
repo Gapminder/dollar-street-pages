2f42fbc54b9360dcbba08f3f47c8d37b8fcc3450
Nepipenkoigor <igornepipenko@gmail.com>
2016-02-15 16:23:17 +0200

    Update: add matrix images component

--- app/matrix/matrix.component.ts
+++ app/matrix/matrix.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from 'angular2/core';
-import { Router } from 'angular2/router';
+import { Component, OnInit ,Inject} from 'angular2/core';
+
+import {MatrixService} from './matrix.service';
+import {MatrixImagesComponent} from './matrix.images.component/matrix.images.component';
 
 let tpl = require('./matrix.component.html');
 let style = require('./matrix.component.css');
@@ -7,8 +9,23 @@ let style = require('./matrix.component.css');
 @Component({
   selector: 'matrix',
   template: tpl,
-  styles: [style]
+  styles: [style],
+  directives:[MatrixImagesComponent]
 })
-export class MatrixComponent {
-
+export class MatrixComponent implements OnInit{
+  public matrixService:MatrixService;
+  public places:any[]=[];
+  constructor(@Inject(MatrixService) matrixService){
+    this.matrixService=matrixService;
+    console.log(this.matrixService)
+  }
+  ngOnInit(): void {
+    this.matrixService.getMatrixImages(`thing=546ccf730f7ddf45c0179688&regions=World&countries=World`)
+      .subscribe((res)=>{
+        if(res.err){
+          return res.err;
+        }
+        this.places=res.places;
+      });
+  }
 }
