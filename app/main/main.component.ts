6354450cb3c98655fd26d0bf5626e2f9d4944d4c
nutman <juncker88@mail.ru>
2016-02-16 14:48:14 +0200

    New: added things component to main page

--- app/main/main.component.ts
+++ app/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit ,Inject} from 'angular2/core';
 import {HeaderMainComponent} from './header/header.main.component';
 import {ComparisonMainComponent} from './comparison/comparison.main.component';
+import {ThingsMainComponent} from './things/things.main.component';
 import {AboutMainComponent} from './about/about.main.component';
 import {FooterComponent} from '../common/footer/footer.component';
 import {PlacesMainComponent} from './places/places.main.component';
@@ -14,7 +15,7 @@ let style = require('./main.component.css');
   template: tpl,
   styles: [style],
   directives: [HeaderMainComponent, FooterComponent,
-    AboutMainComponent, ComparisonMainComponent,PlacesMainComponent]
+    AboutMainComponent, ComparisonMainComponent, PlacesMainComponent, ThingsMainComponent]
 })
 
 export class MainComponent {
---
+++ app/main/things/things.main.service.ts
@@ -0,0 +1,18 @@
+import {Inject} from 'angular2/core';
+import {Http} from 'angular2/http';
+import {Observable} from "rxjs/Observable";
+
+export class ThingsMainService{
+  public http:Http;
+
+  constructor(@Inject(Http) http:any) {
+    this.http = http;
+  }
+
+  public getMainThings(query:any):Observable<any> {
+    return this.http.get(`http://localhost/consumer/api/v1/main/things`).map((res:any)=>{
+      let parseRes=JSON.parse(res._body);
+      return {err:parseRes.error, things:parseRes.data}
+    })
+  }
+}
