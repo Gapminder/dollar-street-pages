08ac95c57bf2f9fd30b5bd1460bf62b6341885d7
nutman <juncker88@mail.ru>
2016-02-16 20:00:52 +0200

    New: added concept component

---
+++ app/main/concept/concept.main.component.ts
@@ -0,0 +1,63 @@
+import { Component, OnInit, Inject } from 'angular2/core';
+
+import {ConceptMainService} from './concept.main.service';
+
+let tpl = require('./concept.main.component.html');
+let style = require('./concept.main.component.css');
+
+@Component({
+  selector: 'concept-main',
+  template: tpl,
+  styles: [style],
+  providers: [ConceptMainService]
+
+})
+
+export class ConceptMainComponent {
+  public conceptMainService:ConceptMainService;
+  public activeThing:any = {};
+  public amazonS3Url:any = 'http://static.dollarstreet.org.s3.amazonaws.com/';
+  public images:any[] = [];
+  public things:any[] = [];
+  public houseClassForRegion:any = {
+    Asia: 'asia-house',
+    Africa: 'africa-house',
+    Europe: 'europe-house',
+    America: 'america-house'
+  };
+
+  constructor(@Inject(ConceptMainService) conceptMainService:any) {
+    this.conceptMainService = conceptMainService;
+  }
+
+  ngOnInit() {
+    this.conceptMainService.getMainConceptThings({})
+      .subscribe((res:any)=> {
+        if (res.err) {
+          return res.err;
+        }
+        this.things = res.things;
+
+        let lastThing = this.things[this.things.length - 1];
+
+        this.activeThing = {_id: lastThing._id, plural: lastThing.plural, icon: lastThing.thingUrl};
+        this.renderIncomePlot(this.activeThing);
+      });
+  }
+
+  selectThing(thing:any) {
+    this.activeThing = {_id: thing._id, plural: thing.plural, icon: thing.thingUrl};
+    this.renderIncomePlot(thing);
+  };
+
+  renderIncomePlot(thing:any) {
+    this.conceptMainService.getMainConceptImages({thingId: thing._id})
+      .subscribe((res:any)=> {
+        if (res.err) {
+          return res.err;
+        }
+
+        this.images = res.images;
+      });
+  }
+}
