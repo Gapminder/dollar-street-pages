42b557c1866841ba02813bea871fa96cdc4d090e
Yura Betrozov <betrozov91@gmail.com>
2016-02-16 19:02:05 +0200

    Update: added fonts on all pages

--- app/main/things/things.main.component.ts
+++ app/main/things/things.main.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit ,Inject } from 'angular2/core';
-
-import {ThingsMainService} from './things.main.service';
+import {Component, Inject} from 'angular2/core';
+import {ThingsMainService} from './things.main.service'
 
 let tpl = require('./things.main.component.html');
 let style = require('./things.main.component.css');
@@ -14,20 +13,20 @@ let style = require('./things.main.component.css');
 
 export class ThingsMainComponent {
   public thingsMainService:ThingsMainService;
-  public things:any[]=[];
+  public things:any[] = [];
   public amazonUrl:any;
 
-  constructor(@Inject(ThingsMainService) thingsMainService:any){
-    this.thingsMainService=thingsMainService;
-  }
-  ngOnInit(): void {
-    this.thingsMainService.getMainThings({})
-      .subscribe((res: any)=>{
-        if(res.err){
-          return res.err;
-        }
-        this.things=res.things;
-      });
+  constructor(@Inject(ThingsMainService) thingsMainService:any) {
+    this.thingsMainService = thingsMainService;
   }
 
+  ngOnInit():void {
+    this.thingsMainService.getMainThings({})
+      .subscribe((res:any)=> {
+        if (res.err) {
+          return res.err;
+        }
+        this.things = res.things;
+      });
+  }
 }
