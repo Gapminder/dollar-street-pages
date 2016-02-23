7a8b1ce0ecea7a039d28769b49d049a4aa50ed40
nutman <juncker88@mail.ru>
2016-02-23 15:15:25 +0200

    Update: added connection for share button component with backend

--- app/common/social_share_buttons/social_share_buttons.service.ts
+++ app/common/social_share_buttons/social_share_buttons.service.ts
@@ -1,18 +1,19 @@
-import {Injectable} from 'angular2/core';
-import {Location} from 'angular2/router';
+import {Inject} from 'angular2/core';
+import {Http} from 'angular2/http';
+import {Observable} from "rxjs/Observable";
 
-@Injectable()
 export class SocialShareButtonsService {
 
-  constructor(location: Location) {}
+  public http:Http;
 
-  getUrl() {
+  constructor(@Inject(Http) http:Http) {
+    this.http = http;
+  }
 
-    /**
-     * todo: complete shorten url
-     */
-    console.log(location.href);
-
-    return 'hello world'
+  public getUrl(query:any):Observable<any> {
+    return this.http.get(`http://localhost/consumer/api/v1/shorturl?${query}`).map((res:any)=>{
+      let parseRes=JSON.parse(res._body);
+      return {err:parseRes.error, url:parseRes.data}
+    })
   }
 }
