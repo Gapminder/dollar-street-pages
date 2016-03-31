027f06da9935f50905eb90a2db1e891670c739a5
Yura Betrozov <betrozov91@gmail.com>
2016-03-31 19:31:13 +0300

    feat(component): add back to top

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject} from 'angular2/core';
-import {RouteConfig, RouterOutlet} from 'angular2/router';
+import {RouteConfig, Router, RouterOutlet} from 'angular2/router';
 import {config} from './app.config';
 
 import {Angulartics2GoogleAnalytics} from 'angulartics2/providers/angulartics2-google-analytics';
@@ -13,10 +13,16 @@ import {Angulartics2GoogleAnalytics} from 'angulartics2/providers/angulartics2-g
 @RouteConfig(config.routes)
 
 export class AppComponent {
-  type:string = 'app component';
+  private router:Router;
   private angulartics2GoogleAnalytics:Angulartics2GoogleAnalytics;
+  type:string = 'app component';
 
-  constructor(@Inject(Angulartics2GoogleAnalytics) angulartics2GoogleAnalytics) {
+  constructor(@Inject(Router) router,
+              @Inject(Angulartics2GoogleAnalytics) angulartics2GoogleAnalytics) {
     this.angulartics2GoogleAnalytics = angulartics2GoogleAnalytics;
+
+    router.subscribe(() => {
+      document.body.scrollTop = 0;
+    });
   }
 }
