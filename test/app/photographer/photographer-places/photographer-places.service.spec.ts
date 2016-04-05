e78768b38de066dfa02c0168cf61d2a1307cb909
nutman <juncker88@mail.ru>
2016-04-05 16:00:20 +0300

    feature(photographer/test): add test for photographer component
    add tests
    ToDo: bring some enhancements to the tests
    
    No breaking changes

---
+++ test/app/photographer/photographer-places/photographer-places.service.spec.ts
@@ -0,0 +1,67 @@
+import {
+  it,
+  describe,
+  expect,
+  inject,
+  fakeAsync,
+  beforeEachProviders,
+  tick,
+} from 'angular2/testing';
+
+import {MockBackend} from 'angular2/http/testing';
+import {provide} from 'angular2/core';
+import {
+  Http,
+  ConnectionBackend,
+  BaseRequestOptions,
+  Response,
+  ResponseOptions
+} from 'angular2/http';
+
+import {config} from '../../../../app/app.config.ts';
+
+import {PhotographerPlacesService} from '../../../../app/photographer/photographer-places/photographer-places.service';
+
+describe('PhotographerPlacesService', () => {
+  beforeEachProviders(() => {
+    return [
+      BaseRequestOptions,
+      MockBackend,
+      PhotographerPlacesService,
+      provide(
+        Http, {
+          useFactory: (backend:ConnectionBackend, defaultOptions:BaseRequestOptions) => {
+            return new Http(backend, defaultOptions);
+          }, deps: [MockBackend, BaseRequestOptions]
+        })
+    ];
+  });
+  it('test getPhotographers()', inject([PhotographerPlacesService, MockBackend], fakeAsync((photographerPlacesService,
+                                                                                       mockBackend) => {
+    var res;
+    mockBackend.connections.subscribe(connection => {
+      expect(connection.request.url).toBe(`${config.api}/consumer/api/v1/photographer-places?id=56ec091caf72e9437cbccfab`);
+      let response = new ResponseOptions({
+        body: `{"success":true,"msg":[],"data":{"countries":[{"name":"India","region":"Asia","places":
+        [{"image":"http://static.dollarstreet.org.s3.amazonaws.com/media/India 2/image/62f004cd-0ede-4a2c-bd57-6b8f4815baba/thumb-62f004cd-0ede-4a2c-bd57-6b8f4815baba.jpg",
+        "family":"Ramachandran","placeId":"54b51b835edc101155fa1ed2","income":100,"imageId":"54b51ba75edc101155fa1ed7","thing":"546ccf730f7ddf45c017962f"},
+        {"image":"http://static.dollarstreet.org.s3.amazonaws.com/media/India 1/image/db6192ba-963f-48ba-ae73-1ece7c505109/thumb-db6192ba-963f-48ba-ae73-1ece7c505109.jpg",
+        "family":"Ganesh","placeId":"54b51a173755cbfb542c2473","income":96,"imageId":"54b51a573755cbfb542c247a","thing":"546ccf730f7ddf45c017962f"},
+        {"image":"http://static.dollarstreet.org.s3.amazonaws.com/media/India 3/image/42beb78b-0e1c-405f-9dc4-64c478de51aa/thumb-42beb78b-0e1c-405f-9dc4-64c478de51aa.jpg",
+        "family":"Abdul Kadhar","placeId":"54b51c593755cbfb542c24a5","income":145,"imageId":"54b51c8105df73e55431911b","thing":"546ccf730f7ddf45c017962f"},
+        {"image":"http://static.dollarstreet.org.s3.amazonaws.com/media/India 4/image/2d763a64-285b-4bd7-862e-cc1d3cb25132/thumb-2d763a64-285b-4bd7-862e-cc1d3cb25132.jpg",
+        "family":"Gada","placeId":"54b520ed05df73e55431912b","income":96,"imageId":"54b5213d38ef07015525f1bd","thing":"546ccf730f7ddf45c017962f"}]}],
+        "familyThingId":"546ccf730f7ddf45c017962f"},"error":null}`
+      });
+      connection.mockRespond(new Response(response));
+    });
+    photographerPlacesService.getPhotographerPlaces('id=56ec091caf72e9437cbccfab').subscribe((_res) => {
+      res = _res;
+    });
+    tick();
+    expect(res.err).toBe(null);
+    expect(res.data.countries.length).toBe(1);
+    expect(res.data.countries[0].places.length).toBe(4);
+  })));
+});
+
