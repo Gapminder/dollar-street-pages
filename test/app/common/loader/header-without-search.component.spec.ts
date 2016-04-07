f26b6ce79642876b4aa51294afbf12864d087e0c
nutman <juncker88@mail.ru>
2016-04-07 13:02:20 +0300

    test(loader): add test for loader
    simple test
    
    No breaking changes

---
+++ test/app/common/loader/header-without-search.component.spec.ts
@@ -0,0 +1,27 @@
+import {
+  it,
+  describe,
+  expect,
+  injectAsync,
+  beforeEachProviders,
+  TestComponentBuilder,
+} from 'angular2/testing';
+
+import {MockCommonDependency} from '../../../app/common-mocks/mocked.services';
+import {LoaderComponent} from '../../../../app/common/loader/loader.component';
+
+describe('LoaderComponent', () => {
+  let mockCommonDependency = new MockCommonDependency();
+  beforeEachProviders(() => {
+    return [
+      mockCommonDependency.getProviders()
+    ];
+  });
+  it('LoaderComponent must init', injectAsync([TestComponentBuilder], (tcb) => {
+    return tcb.createAsync(LoaderComponent).then((fixture) => {
+      let context = fixture.debugElement.componentInstance;
+      fixture.detectChanges();
+      expect(context.top).toBe(0);
+    });
+  }));
+});
