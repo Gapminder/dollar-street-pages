1b04612c32f5e34cd109dc5a8932eded91549544
ludmilanesvitiy <ludmila.nesvitiy@valor-software.com>
2016-04-14 11:50:51 +0300

    Adding new tests at Photographers Page

--- test-e2e/app/photographer/photographer.page.e2e.ts
+++ test-e2e/app/photographer/photographer.page.e2e.ts
@@ -1,62 +1,53 @@
-/**
- * Created by vs on 11/4/16.
- */
-describe('Photographers Page ', function() {
+describe('Photographer Page ', function() {
   //  jasmine.DEFAULT_TIMEOUT_INTERVAL = 100000;
 
   var searchField = element(by.css('#search'));
   var EC = protractor.ExpectedConditions;
   var TIMEOUT = 10000;
   var zorianMiller = element(by.css('.photographer-card[href="/photographer?id=56e946bed360263447ff6f95"]'));
-  var message = 'Zorian Miller on PhotographersPage is not visibility';
-  var photographerPortreit = element(by.css('.photographer-portrait'));
+  var photographerPortreit = element(by.css('.header>img'));
   var photographerMessage = 'Photographer portrait is not visible';
-  var country = element(by.css('.family>h3'));
+  var families = element(by.css('.family>h3'));
   var totalPhotos = element(by.css('.photo'));
-  var totalVideos = element(by.css('.camera'));
-  var namePhotographer = element(by.css('.header>h2'));
+  var namePhotographer = element.all(by.css('.header>h2')).first();
+  var country = element(by.css('.place-country>p>span'));
   browser.manage().window().maximize();
-  beforeEach(function() {
-    browser.get('/photographers');
-    browser.wait(EC.visibilityOf(zorianMiller), TIMEOUT, message);
-  });
-  afterEach (function check() {
-    /*browser.wait(EC.visibilityOf(photographerPortreit), TIMEOUT, photographerMessage);
-    photographerPortreit.click();*/
-    browser.isElementPresent(namePhotographer);
-    browser.isElementPresent(country);
-    browser.isElementPresent(totalPhotos);
-    browser.isElementPresent(totalVideos);
-  });
-  /*afterAll(function() {
-    browser.close();
-  });*/
   /* Open Photographers Page
    * Click on the Photographer
    * Check existence name, Visited Families, Photos, Videos
    */
+  beforeEach(function() {
+    browser.get('/photographers');
+    browser.wait(EC.visibilityOf(zorianMiller), TIMEOUT, 'Zorian Miller on PhotographersPage is not visible');
+  });
   it('test AJ SHARMA', function() {
     searchField.sendKeys('AJ SHARMA' + '\n');
+    element(by.css('.photographer-card>h3')).click();
   });
   it('test Anna Graboowska', function() {
-    browser.get('/photographer?id=56ec091caf72e9437cbccfab');
+    browser.get('/photographer?id=56e946bad360263447ff6f89');
   });
   it('test Zoriah Miller', function() {
-    browser.get('/photographer?id=56ec0917af72e9437cbccf93');
+    browser.get('/photographer?id=56e946bed360263447ff6f95');
   });
   it('test Luc Forsyth', function() {
-    browser.get('/photographer?id=56ec0918af72e9437cbccf97');
+    browser.get('/photographer?id=56e946bfd360263447ff6f99');
   });
   it('test Jonathan Taylor', function() {
-    browser.get('/photographer?id=56ec0917af72e9437cbccf94');
+    browser.get('/photographer?id=56e946bed360263447ff6f96');
   });
   it('test Abhineet Malhotra', function() {
-    browser.get('/photographer?id=56ec0919af72e9437cbccf9b');
+    browser.get('/photographer?id=56e946c0d360263447ff6f9d');
   });
   it('test Konstatins Sigulis', function() {
-    browser.get('/photographer?id=56ec091caf72e9437cbccfac');
+    browser.get('/photographer?id=56e946c5d360263447ff6fae');
   });
-  /*it('test Jose Ramirez', function() {
-    searchField.sendKeys('Jose Ramirez\n');
-  });*/
+  afterEach (function check() {
+    browser.wait(EC.visibilityOf(photographerPortreit), TIMEOUT, photographerMessage);
+    browser.wait(EC.visibilityOf(totalPhotos), TIMEOUT, 'Icon Total Photos on Photographer Page is not visible');
+    browser.wait(EC.visibilityOf(country), TIMEOUT, 'Countries on Photographer Page are not visible');
+    expect(namePhotographer.isDisplayed()).toBe(true);
+    expect(families.isDisplayed()).toBe(true);
+    expect(totalPhotos.isDisplayed()).toBe(true);
+  });
 });
