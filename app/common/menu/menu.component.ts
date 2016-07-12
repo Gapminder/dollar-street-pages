d3efb5c01cae5ed335ff0453958f5f22f419e591
Maxim Ugnichenko <ugmaxie@gmail.com>
2016-07-12 17:58:53 +0300

    fix(main-menu): code modifying for proper reload page use

--- app/common/menu/menu.component.ts
+++ app/common/menu/menu.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, OnDestroy, HostListener, Inject, ElementRef } from '@angular/core';
-import { RouterLink } from '@angular/router-deprecated';
+import { RouterLink, Router } from '@angular/router-deprecated';
 import { Observable } from 'rxjs/Observable';
 import { SocialShareButtonsComponent } from '../social_share_buttons/social-share-buttons.component.ts';
 
@@ -19,9 +19,12 @@ export class MainMenuComponent implements OnInit, OnDestroy {
   private hoverPlace:Observable<any>;
   private hoverPlaceSubscribe:any;
   private element:ElementRef;
+  private router:Router;
 
-  public constructor(@Inject(ElementRef) element:ElementRef) {
+  public constructor(@Inject(Router) router:Router,
+                     @Inject(ElementRef) element:ElementRef) {
     this.element = element;
+    this.router = router;
   }
 
   public ngOnInit():void {
@@ -39,10 +42,14 @@ export class MainMenuComponent implements OnInit, OnDestroy {
   }
 
   protected switchOnOnboardingFromMenu():void {
-    if (window.localStorage && window.localStorage.getItem('onboarded')) {
+    let ifOnMatrixPage = this.router.hostComponent.name === 'MatrixComponent';
+
+    if (ifOnMatrixPage) {
       window.localStorage.removeItem('onboarded');
+      location.reload();
     }
-    location.reload();
+    window.localStorage.removeItem('onboarded');
+    this.router.navigate(['Matrix']);
   }
 
   @HostListener('document:click', ['$event'])
