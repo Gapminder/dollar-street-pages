1905d8b55d26397b8636ce520a94456e25964c04
Dmitriy Mihaylenko <dmitriy.mihaylichenko@valor-software.com>
2017-05-30 14:53:23 +0300

    fix(family-header): current language fix (#825)

--- src/family/family-header/family-header.component.ts
+++ src/family/family-header/family-header.component.ts
@@ -52,6 +52,7 @@ export class FamilyHeaderComponent implements OnInit, OnDestroy {
   public isTablet: boolean;
   public getTranslationSubscribe: Subscription;
   public languageService: LanguageService;
+  public currentLanguage: string;
   public showTranslateMe: boolean;
 
   public constructor(zone: NgZone,
@@ -74,6 +75,8 @@ export class FamilyHeaderComponent implements OnInit, OnDestroy {
     this.isDesktop = this.device.isDesktop();
     this.isMobile = this.device.isMobile();
     this.isTablet = this.device.isTablet();
+
+    this.currentLanguage = this.languageService.currentLanguage;
 
     this.headerElement = document.querySelector('.header-container') as HTMLElement;
     this.headerContentHeight = this.element.offsetHeight;
