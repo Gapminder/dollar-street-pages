00aa2b455eccebc1a6804e1e41091a4fe1af41f6
mdmitriy <dmitriy.mihaylichenko@valor-software.com>
2017-06-08 16:03:29 +0300

    chor(environment): move env option

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
+import { Config } from './app.config';
 import { AppModule } from './app/app.module';
 
-if (environment.production) {
+if (Config.production) {
   enableProdMode();
 }
 
