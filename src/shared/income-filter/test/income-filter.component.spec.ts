d0683c44f6af138372cc4b9ad61c643334671e6f
Dmitriy Mihaylenko <dmitriy.mihaylichenko@valor-software.com>
2017-08-17 16:56:57 +0300

    test(income-filter): add test for income-filter (#920)

---
+++ src/shared/income-filter/test/income-filter.component.spec.ts
@@ -0,0 +1,55 @@
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { TranslateModule, TranslateService, TranslateLoader, TranslateParser } from 'ng2-translate';
+import { StoreModule } from '@ngrx/store';
+import {
+    MathService
+} from '../../../common';
+import { StreetFilterComponent } from '../../street-filter/street-filter.component';
+import { StreetFilterDrawService } from '../../street-filter/street-filter.service';
+import { IncomeFilterComponent } from '../income-filter.component';
+
+describe('IncomeFilterComponent', () => {
+    let fixture: ComponentFixture<IncomeFilterComponent>;
+    let component: IncomeFilterComponent;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                TranslateModule,
+                StoreModule.forRoot({})
+            ],
+            declarations: [
+                IncomeFilterComponent,
+                StreetFilterComponent
+            ],
+            providers: [
+                MathService,
+                StreetFilterDrawService,
+                TranslateService,
+                TranslateLoader,
+                TranslateParser
+            ]
+        });
+
+        fixture = TestBed.createComponent(IncomeFilterComponent);
+        component = fixture.componentInstance;
+    }));
+
+    it('ngAfterViewInit()', () => {
+        component.ngAfterViewInit();
+
+        expect(component.streetSettingsStateSubscription).toBeDefined();
+
+        spyOn(component.streetSettingsStateSubscription, 'unsubscribe');
+
+        component.ngOnDestroy();
+
+        expect(component.streetSettingsStateSubscription.unsubscribe).toHaveBeenCalled();
+    });
+
+    it('initData()', () => {
+        component.ngAfterViewInit();
+
+        component.initData();
+    });
+});
