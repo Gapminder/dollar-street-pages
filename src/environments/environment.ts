ff0d4e4b90481e2ea54741083814dbf1a81bc804
mdmitriy <dmitriy.mihaylichenko@valor-software.com>
2017-09-05 15:10:11 +0300

    fix(countries-filter): fix for countries filter mobile

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   // consumerApi: '//consumer-api-dev.dollarstreet.org',
   // consumerApi: '//localhost:8015',
-  consumerApi: 'http://54.171.239.110',
+  consumerApi: 'http://52.212.100.56',
   stripPublicKey: 'pk_test_t5Ct4SMWKOD7uycxzbmaerp3'
 };
