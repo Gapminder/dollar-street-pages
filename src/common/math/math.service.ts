42a30e27af7cebd0fa93a0eedb2bca0a3371849f
Dmitriy Mihaylenko <dmitriy.mihaylichenko@valor-software.com>
2017-10-19 15:38:07 +0300

    fix(exp-impr): fix for math service (#1075)

--- src/common/math/math.service.ts
+++ src/common/math/math.service.ts
@@ -5,4 +5,8 @@ export class MathService {
   public roundIncome(income: number = 0, decimal: boolean = false): string | number {
     return decimal ? income.toFixed(2) : Math.round(income).toString();
   }
+
+  public round(income: number = 0): string | number {
+    return Math.round(income).toString();
+  }
 }
