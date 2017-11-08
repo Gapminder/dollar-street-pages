252c54a9868412a41f478291ece11fbea3b118d1
Sergey Khristenko <sergey.khristenko@valor-software.com>
2017-11-08 19:11:58 +0200

    test: create common testing module with services

---
+++ src/test/mocks/appTestModule.mock.ts
@@ -0,0 +1,9 @@
+import { BlankComponent } from './blank.component.mock';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [BlankComponent],
+  exports: [BlankComponent]
+})
+export class AppTestModule {
+}
---
+++ src/test/mocks/titleHeader.service.mock.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class TitleHeaderServiceMock {
+  // tslint:disable-next-line
+  public setTitle(title: string): void {
+  }
+}
