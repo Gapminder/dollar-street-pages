d713905588f3ae84149cb1bf3b486b325c84afda
Sergey Khristenko <sergey.khristenko@valor-software.com>
2017-12-11 17:26:57 +0200

    test: separate integration and unit tests

--- src/photographers/test/photographers.component.spec.ts
+++ src/photographers/test/photographers.component.spec.ts
@@ -13,11 +13,9 @@ import { PhotographersFilterPipe } from '../photographers-filter.pipe';
 import { CommonServicesTestingModule } from '../../test/commonServicesTesting.module';
 import { TranslateTestingModule } from '../../test/translateTesting.module';
 
-describe('Component: PhotographersComponent', () => {
+fdescribe('Component: PhotographersComponent', () => {
   let component: PhotographersComponent;
   let fixture: ComponentFixture<PhotographersComponent>;
-  let photographersService: PhotographersServiceMock;
-  let router: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -39,8 +37,6 @@ describe('Component: PhotographersComponent', () => {
 
     fixture = TestBed.createComponent(PhotographersComponent);
     component = fixture.componentInstance;
-    photographersService = TestBed.get(PhotographersService);
-    router = TestBed.get(Router);
   });
 
   it('define subscriptions on ngAfterViewInit()', () => {
@@ -64,116 +60,6 @@ describe('Component: PhotographersComponent', () => {
     expect(component.getTranslationSubscribe.unsubscribe).toHaveBeenCalled();
     expect(component.photographersServiceSubscribe.unsubscribe).toHaveBeenCalled();
   });
-
-  xit('should blur search input on mobile', () => {
-    // TODO how to test this?
-    component.isDesktop = false;
-  });
-
-  it('toggleLeftSide should toggle sidebar', () => {
-    spyOn(component, 'toggleLeftSide');
-    component.ngAfterViewInit();
-
-    component.isDesktop = false;
-    const toggleBtn = fixture.debugElement.query(By.css('.sort-country'))
-      .nativeElement
-      .click();
-
-    expect(component.toggleLeftSide).toHaveBeenCalled();
-  });
-
-  describe('Photographer card created on init', () => {
-    beforeEach(() => {
-      spyOn(photographersService, 'getPhotographers').and.returnValue(Observable.of({
-        data: {
-          countryList: countryListData,
-          photographersList: photographerListData
-        }
-      }));
-      spyOn(router, 'navigate').and.stub();
-
-      component.ngAfterViewInit();
-      fixture.detectChanges();
-    });
-
-    it('Cards created', () => {
-      const cards = fixture.debugElement.queryAll(By.css('.photographer-card'));
-
-      expect(cards.length).toEqual(photographerListData.length);
-    });
-
-    it('Avatar created', () => {
-      const cardAvatar = fixture.debugElement.query(By.css('.photographer-portrait'));
-
-      expect(cardAvatar.styles['background-image']).toEqual(photographerListData[0].avatar);
-    });
-
-    it('Card name set', () => {
-      const cardName = fixture.debugElement.query(By.css('h3')).nativeElement;
-
-      expect(cardName.textContent).toEqual(photographerListData[0].name);
-    });
-
-    it('show number of photographer images', () => {
-      const imagesCounter = fixture.debugElement.query(By.css('.photographer-material span')).nativeElement;
-
-      expect(Number(imagesCounter.textContent)).toEqual(photographerListData[0].images);
-    });
-
-    it('show number of photographer videos', () => {
-      const videosCounter = fixture.debugElement.queryAll(By.css('.photographer-material span'))[1].nativeElement;
-
-      expect(Number(videosCounter.textContent)).toEqual(photographerListData[0].video);
-    });
-
-    it('show number of photographer places', () => {
-      const placesCounter = fixture.debugElement.queryAll(By.css('.photographer-material span'))[2].nativeElement;
-
-      expect(Number(placesCounter.textContent)).toEqual(photographerListData[0].places);
-    });
-
-    xit('should navigate to photographer page on click', () => {
-      // TODO how to test routerLink ???
-      const photographerCard = fixture.debugElement.query(By.css('.photographer-card'))
-        .nativeElement
-        .click();
-
-      expect(router.navigate).toHaveBeenCalled();
-    });
-  });
-
-  describe('Country list created on init', () => {
-    beforeEach(() => {
-      spyOn(photographersService, 'getPhotographers').and.returnValue(Observable.of({
-        data: {
-          countryList: countryListData,
-          photographersList: photographerListData
-        }
-      }));
-      spyOn(router, 'navigate').and.stub();
-
-      component.ngAfterViewInit();
-      fixture.detectChanges();
-    });
-
-    it('Country list created', () => {
-      const countries = fixture.debugElement.queryAll(By.css('.country-card'));
-
-      expect(countries.length).toEqual(countryListData.length);
-    });
-
-    it('Photographer country created', () => {
-      const country = fixture.debugElement.query(By.css('.photographer-country')).nativeElement;
-
-      expect(country.textContent).toEqual(countryListData[0].name);
-    });
-
-    it('Photographers_names_by_countries created', () => {
-      const photographerName = fixture.debugElement.query(By.css('.photographers-list li')).nativeElement;
-
-      expect(photographerName.textContent.trim()).toEqual(countryListData[0].photographers[0].name);
-    });
-  });
 });
 
 class PhotographersServiceMock {
@@ -186,39 +72,3 @@ class PhotographersServiceMock {
     });
   }
 }
-
-const countryListData = [
-  {
-    name: 'CountryName',
-    photographers: [{
-      avatar: 'country1',
-      images: 1,
-      name: 'country1',
-      places: 1,
-      userId: 'country1'
-    }, {
-      avatar: 'country2',
-      images: 2,
-      name: 'country2',
-      places: 2,
-      userId: 'country2'
-    }]
-  }];
-
-const photographerListData = [
-  {
-    avatar: 'photographer1',
-    images: 10,
-    name: 'photographer1',
-    places: 2,
-    userId: 'photographer1',
-    video: 1
-  },
-  {
-    avatar: 'photographer1',
-    images: 22,
-    name: 'photographer1',
-    places: 3,
-    userId: 'photographer1',
-    video: 2
-  }];
