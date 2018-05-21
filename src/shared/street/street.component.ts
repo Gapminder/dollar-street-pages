9f9a61745f4822b5a7fbff99be1574881607a4e2
Mikhail Budarin <miffael.jo@gmail.com>
2018-05-21 11:11:00 +0300

    fix(DS): fix dividers on results outside filters sets and hide float footer for langscape small devices (#1318)

--- src/shared/street/street.component.ts
+++ src/shared/street/street.component.ts
@@ -145,7 +145,6 @@ export class StreetComponent implements OnDestroy, AfterViewInit {
     });
 
     this.streetFilterSubscribe = this.street.filter.subscribe((filter: IncomeFilter): void => {
-
       this.street.set('lowIncome', filter.lowIncome);
       this.street.set('highIncome', filter.highIncome);
 
