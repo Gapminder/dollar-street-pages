787d286c7bb1fe846041f7d1a60df6bed2bcda1e
Yuriy777 <yuriy777kravchenko@gmail.com>
2018-10-25 11:37:19 +0300

    feat(matrix): remove metadata from full screen images

--- src/family/family-media/family-media-view-block/family-media-view-block.component.ts
+++ src/family/family-media/family-media-view-block/family-media-view-block.component.ts
@@ -257,7 +257,7 @@ export class FamilyMediaViewBlockComponent implements OnInit, OnChanges, OnDestr
   openPopUp(): void {
     this.popIsOpen = true;
 
-    const imgUrl = this.imageData.image.replace(this.imageResolution.expand, this.imageResolution.full);
+    const imgUrl = this.consumerApi + '/v1/download-image/' + this.imageData.imageId;
     const newImage = new Image();
 
     newImage.onload = () => {
